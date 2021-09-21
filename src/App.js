838652c5cdb0ce64931b388fa6d5982e4096b3d7
Monkie_J <anhanh1592@gmail.com>
2021-09-21 19:58:33 +0200

    BMI calculator with reactjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Form from "./Form";
+import Message from "./Message";
 
 function App() {
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+  const [BMI, setBMI] = useState("");
+  const [notice, setNotice] = useState("");
+
+  useEffect (() =>{
+    messageOutput();
+  }, [BMI]);
+
+  const messageOutput = () => {
+    if(BMI === ""){
+      setNotice("");
+    }
+    else if(BMI<18.5) {
+      setNotice("Underweight! You should eat more!")
+    }
+    else if(18.5 <= BMI && BMI < 24.9) {
+      setNotice("You have a great BMI! Keep going!")
+    }
+    else if(24.9 <= BMI && BMI < 29.9) {
+      setNotice("You are Overweight! You should go on a diet!")
+    }
+    else {
+      setNotice("Dangerous Zone! !!!You are obese!!!");
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header><h1>Monkie_J BMI Calculator</h1></header>
+      <Form
+        weight = {weight}
+        setWeight = {setWeight}
+        height = {height}
+        setHeight = {setHeight}
+        setBMI = {setBMI}
+      ></Form>
+      <Message
+        BMI = {BMI}
+        notice = {notice}
+      ></Message>
     </div>
   );
 }
